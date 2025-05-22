edcaf12e447f31e69daf155571028283f24fca4c
Priyobrato.Das <priyodas12@gmail.com>
2025-05-22 21:42:09 +0530

    traverse string elements

---
+++ strings/_01_basics.py
@@ -0,0 +1,19 @@
+s1="hello"
+s2=str(123)
+print(f"example of strings: {s1}, {s2}")
+
+name="priyobrato"
+mail_broker="gmail.com"
+
+print(f"my email: {name}@{mail_broker}")
+
+# access string
+
+s3='hello world!'
+print(s3[0])
+print(s3[-1]) # last index
+print(s3[0:8:2])  # traverse with skip
+
+# get string length
+print(len(s3))
+
---
+++ strings/_02_traverse_string.py
@@ -0,0 +1,15 @@
+s3="random letter for traverse!"
+# string traverse
+
+# with for loop: you just need each character and don't care about the index.
+for x in s3:
+    print(x)
+
+# range function
+# You need the index, for example:
+    # Modifying characters (in a list version of the string).
+    # Comparing with neighboring characters.
+    # Tracking position.
+
+for y in range(0,len(s3)):
+    print(s3[y])
