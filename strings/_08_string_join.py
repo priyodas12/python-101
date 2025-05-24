3994d7b10bb1c24a9e80dcd5123c1737c4b9b946
Priyobrato Das <priyodas12@gmail.com>
2025-05-24 20:18:38 +0530

    different split and replace

---
+++ strings/_08_string_join.py
@@ -0,0 +1,20 @@
+# replace old subString and new subString
+
+s1="old string archive"
+
+result=s1.replace("old","new");
+print(f'replacement : {result}')
+
+# join string
+words = ["Hello", "world", "from", "Python"]
+result=" _*_ ".join(words)
+print(f'after joining iterable : {result}')
+
+nums = [1, 2, 3]
+result = "-".join(str(num) for num in nums)
+print(f'after joining numbers : {result}')
+
+# split string
+result=s1.split(" ") # reruns list
+print(f'after splitting numbers : {result}')
+print(type(result)) #<class 'list'>
