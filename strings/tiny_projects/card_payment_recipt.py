9523f3cefa32defd5356fc559f7be637048ff15d
Priyobrato Das <priyodas12@gmail.com>
2025-06-02 17:35:12 +0530

    string tiny projects

--- strings/tiny_projects/card_payment_recipt.py
+++ strings/tiny_projects/card_payment_recipt.py
@@ -2,12 +2,16 @@ from datetime import datetime
 # input: 3456-7832-8233-0929:67782.8923:debit,3456-7832-8233-7821:90000.8923:credit
 tx_details=input("enter card_number:transaction_amount").split(",")
 menu_length=30
-
+tx_status_code="";
 for tx_detail in tx_details:
     card_number,tx_amt,tx_status=tx_detail.split(":")
+    if tx_status== 'credit':
+        tx_status_code="to"
+    else:
+        tx_status_code="from"
     print(f"""---------Transaction Alert------------
 Amount {tx_amt} has been {tx_status}ed 
-from Card Number: {card_number}
+{tx_status_code} Card Number: {('X' * (4) +"-")*3+ card_number[-4:]}
 at {datetime.now()}
 --------------------------------------
 """)
