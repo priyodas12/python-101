214201519e87b7ecddd205cbc88dde0b5f3de218
Priyobrato.Das <priyodas12@gmail.com>
2025-06-17 14:35:16 +0530

    positional vs keyword args

--- functions/positional_vs_keyword_args.py
+++ functions/positional_vs_keyword_args.py
@@ -18,3 +18,11 @@ calculate_volume(10,2,height=3)
 
 # SyntaxError: positional argument follows keyword argument
 # calculate_volume(length=12,202,height=3)
+
+def print_seq(a,b,/,c,d,*,e,f):
+    print(f"positional args: {a},{b}")
+    print(f"positional or keywords args: {c},{d}")
+    print(f"keyword args: {e},{f}")
+
+print_seq(1,2,3,d=3,e=99,f=100)
+
