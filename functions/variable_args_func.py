9734272cbb38877a91ed110fbc2202c02df3e982
priyobrato.das <priyodas12@gmail.com>
2025-06-19 09:37:45 +0530

    variable arguments

--- functions/variable_args_func.py
+++ functions/variable_args_func.py
@@ -1,4 +1,4 @@
-# prints args as tuple
+# prints args as tuple, variable args are positional
 def var_argument_func(*args):
     print(args)
 
@@ -10,9 +10,16 @@ def mixed_argument_func(a,b,*args):
 
 mixed_argument_func(True,7.903,6,'y',9.99)
 
-
+# here a,b must be passed as keyword arguments only
 def mix_argument_special_func(*args,a,b):
     print(a,b,args)
 
 # keyword argument followed by args
 mix_argument_special_func(12,22,32,a=90,b=89)
+
+list_of_numbers=[1,5,10,15]
+# entire list made as single args
+var_argument_func(list_of_numbers,10)
+
+#unpacking of list
+var_argument_func(*list_of_numbers,20)
