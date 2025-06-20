e8ad6574c8329e6f5f4551ad319fe854ff3b0d11
priyobrato.das <priyodas12@gmail.com>
2025-06-20 09:17:23 +0530

    global keyword

--- functions/local_and_global_vars.py
+++ functions/local_and_global_vars.py
@@ -2,8 +2,9 @@ g=9.45 # global variable
 print(f"global-1: {g}")
 
 def fun():
+    global g
     a=16 # local variable, accessible inside function only
-    g=902.839 # modifying global var value, scope to this function
+    g=902.839 # modifying global var value, this will alter the value
     print(f"local: {a}, global: {g}")
 
 fun()
