1b041a8fec6a7c18b6c2d93ceb1f76845c5945bb
priyobrato.das <priyodas12@gmail.com>
2025-06-20 10:01:59 +0530

    math operations

---
+++ functions/math/mathmatical_operations.py
@@ -0,0 +1,17 @@
+n=178293.239203
+
+print(abs(-2738)) # always returns positive
+print(abs(-25+8j)) # modules value
+
+print(pow(10,2))
+print(pow(10,-2))
+print(pow(5,2,3)) # after power calculation result % 3(25%3=1)
+
+print(round(n))
+print(round(n,2)) # decimal precision
+
+# special case
+
+print(round(4.5),round(5.5)) # 4,6- Bankers rounding
+
+print(divmod(23,7)) # result - (23/7,23%7)
