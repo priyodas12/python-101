b53a8c0c7a62dcdeacee82b4b25f2c5ed8f23576
priyobrato.das <priyodas12@gmail.com>
2025-06-20 10:07:46 +0530

    math operations

--- functions/math/mathmatical_operations.py
+++ functions/math/mathmatical_operations.py
@@ -1,3 +1,5 @@
+from pygments.lexer import default
+
 n=178293.239203
 
 print(abs(-2738)) # always returns positive
@@ -15,3 +17,13 @@ print(round(n,2)) # decimal precision
 print(round(4.5),round(5.5)) # 4,6- Bankers rounding
 
 print(divmod(23,7)) # result - (23/7,23%7)
+
+num_list=[23.77,21.4552,9,89,0,88,-6]
+
+print(min(num_list))
+print(max(num_list))
+print(min([],default="empty list"))
+
+print(sum(num_list))
+
+print(eval("34*67-23+83/4*526"))
