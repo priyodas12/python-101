7fa8ef959279c4b18c277a84373cfcef217c74cd
priyobrato.das <priyodas12@gmail.com>
2025-06-25 16:20:11 +0530

    exceptions

---
+++ exceptions/different_errors.py
@@ -0,0 +1,10 @@
+l=[1,2,3,4,4]
+#print(l[8])
+#IndexError: list index out of range
+
+test_dict={"1":"test"}
+# print(test_dict["5"])
+# KeyError: '5'
+
+# print(int("ydh"))
+# ValueError: invalid literal for int() with base 10: 'ydh'
--- exceptions/error_types.py
+++ exceptions/error_types.py
@@ -7,15 +7,15 @@ Runtime Error
 
 """
 
-a=input("enter value of a: ")
-b=input("enter value of b: ")
+a=int(input("enter value of a: "))
+b=int(input("enter value of b: "))
 #SyntaxError: unterminated string literal (detected at line 11): typing mistake
 #b=input("enter value of b)
 
-if int(a)>int(b):
+if a>b:
     print("< a greater than b >")
 else:
     #print("< b is greater than a >")
     print("< a greater than b >") # Logical error : use debugger
 
-print(int(a)//int(b)) #ZeroDivisionError: division by zero -- Runtime Error
+print(a//b) #ZeroDivisionError: division by zero -- Runtime Error
