889aa1cbaddc6b5eba0881618588024d46414618
priyobrato.das <priyodas12@gmail.com>
2025-06-25 18:36:46 +0530

    exceptions

---
+++ exceptions/multiple_exception_handling.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+
+num_list=[3,5,8,1,4,7]
+index=0
+try:
+    index = input("please enter index: ")
+    print(f"Printing index: {index} , value: {num_list[int(index)]}")
+except ValueError as msg:
+    print(f"{msg}: \nindex must be integer, actually we received: {index}")
+except IndexError as msg:
+    print(f"{msg}: \ninvalid index value entered: {index}")
+except:
+    print(f"Generic Error")
+finally:
+    print(f"Executed at: {datetime.today()}")
