b9d96f578ec8c96e0223302b8519d8bfea458955
priyobrato.das <priyodas12@gmail.com>
2025-07-24 23:29:23 +0530

    dictionary - allowed key types

--- dictionary/dictionary_impl.py
+++ dictionary/dictionary_impl.py
@@ -19,3 +19,16 @@ for item in dict1:
 # traverse: both key values
 for item in dict1.items():
     print(item)
+
+dict2=dict({1:"one",2:2.0,3:4+7j,4:True,5:[4,5,6],6:(9,6,7),7:{100.992,923.3434,747.734}})
+
+for item in dict2.values():
+    print(type(item),"->",item)
+
+# keys only immutable datatype: list,sets not allowed, only tuple allowed
+# TypeError: unhashable type: 'list'
+# dict3=dict({[1,3,4,5]:"one",21:"21",32:"32"})
+dict3=dict({(1,2):"one,two",3+7j:"21",32.239:"32"})
+
+for item in dict3.keys():
+    print(type(item),"->",item)
